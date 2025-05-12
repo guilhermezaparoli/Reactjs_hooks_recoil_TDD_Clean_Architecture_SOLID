f6b15ff261b69face7c3ba2c02cb556e24d12596
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-05-12 08:29:27 -0300

    fix: typo

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -43,7 +43,7 @@ describe('RemoteAuthentication', () => {
     const { sut, httpPostClientSpy } = makeSut();
 
     httpPostClientSpy.response = {
-      statusCode: HttpStatusCode.unathorized
+      statusCode: HttpStatusCode.unauthorized
     };
     const promise = sut.auth(mockAuthentication());
 
